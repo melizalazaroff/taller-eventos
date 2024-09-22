d9f316cfce15ca21c1d17b0522b00690ff9ac77e
MelizaLazaroff <lazaroffmeliza@gmail.com>
2024-09-22 12:42:47 -0300

    Update eventos.js
    nueva rama y solucion

--- eventos.js
+++ eventos.js
@@ -2,3 +2,9 @@ document.getElementById('div').addEventListener ('click', function() {
     alert('Hola! Soy un div');
 }
 );
+document.getElementById('boton').addEventListener('click',function(event) {
+   
+   
+    event.stopPropagation();
+    alert('Hola!');
+   });
